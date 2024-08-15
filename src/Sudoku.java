e1e24f4798dfaf291ef22fdb4eb2e08f235ea007
Andrew Soozay <asousa2021@gmail.com>
2024-08-15 00:21:14 -0700

    update

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -10,16 +10,17 @@ public class Sudoku {
     }
 
     public void puzzleMaker(){
-        topRightGrid();
+        topLeftGrid();
         printGrid();
+        gridMaker(0,3);
        
     }
 
 
-    // method: topRightGrid();
-    // purpose: creates the top-right grid for puzzle reference
+    // method: topLeftGrid();
+    // purpose: creates the top-left grid for puzzle reference
     // parameters: none
-    public void topRightGrid(){
+    public void topLeftGrid(){
         List<Integer> gridLeft = fullList();
         Random rand = new Random();
 
@@ -36,6 +37,26 @@ public class Sudoku {
     }
 
 
+    public void gridMaker(int rowS, int colS){
+        Random rand = new Random();
+        List<Integer> rowTopPoss = possNum(rowS);
+        List<Integer> rowMiddlePoss = possNum(rowS+1);
+        List<Integer> rowBottomPoss = possNum(rowS+2);
+
+        // use possNum for possible rows, randomly pick a #, check if column has that digit, if not add it and remove possible number from other rows
+
+    }
+
+
+    public static void printPoss(List<Integer> list){
+        System.out.println();
+        System.out.print("Poss. Nums for Row: ");
+        for(int digit : list){
+            System.out.print(digit + " ");
+        }
+        System.out.println();
+    }
+
     // method: fullList (List<Integer>)
     // purpose: creates a list of numbers 1-9, used for grids
     // parameters: none
@@ -45,6 +66,37 @@ public class Sudoku {
             fullList.add(i);
         }
         return fullList;
+    }
+
+
+    // method: possNum (List<Integer>)
+    // purpose: finds the possible numbers for a row in a grid
+    // parameters:  (1) row (int): row to find
+    public List<Integer> possNum(int row){
+        List<Integer> rowUsed = rowUsed(row);
+        List<Integer> fullList = fullList();
+        List<Integer> possNums = new ArrayList<>();
+
+        for(int digit : fullList){
+            if(!rowUsed.contains(digit)){
+                possNums.add(digit);
+            }
+        }
+
+        return possNums;
+    }
+
+
+    // method: rowUsed (List<Integer>)
+    // purpose: finds the digits already used in the row
+    // parameters:  (1) row (int) row to check
+    public List<Integer> rowUsed(int row){
+        List<Integer> rowUsed = new ArrayList<>();
+        for(int col = 0; col <= 8; col++){
+            int digit = puzzle[row][col];
+            rowUsed.add(digit);
+        }
+        return rowUsed;
     }
 
 
