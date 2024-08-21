a7124ac95b882f676ebb6f492c4a11feeef05f2a
ASoozay <asousa2021@gmail.com>
2024-08-20 21:29:29 -0700

    updat

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -10,62 +10,91 @@ public class Sudoku {
     }
 
     public void puzzleMaker(){
-        topRightGrid();
-        printGrid();
-       
+       numberFiller(1);
+       numberFiller(2);
+       numberFiller(3);
+       //numberFiller(4);
+       //numberFiller(5);
+       //numberFiller(6);
+       //numberFiller(7);
+       //numberFiller(8);
+       //numberFiller(9);
+       printPuzzle();
+    }
+
+    public void numberFiller(int digit){
+        // 1) in each grid, check rows for any openings, add to list if have them and row already doesn't have number
+        // 2) randomly pick a row from the list
+        // 3) when have row, check for potential columns, add those that have an opening and dont have number already
+        // 4) randomly pick column from the list
+        // 5) add digit to the spot
+    
+
     }
 
 
-    // method: topRightGrid();
-    // purpose: creates the top-right grid for puzzle reference
-    // parameters: none
-    public void topRightGrid(){
-        List<Integer> gridLeft = fullList();
-        Random rand = new Random();
+    public List<Integer> rowUsed(int row){
+        List<Integer> rowUsed = new ArrayList<>();
+        
+        for(int col = 0; col <= 8; col++){
+            rowUsed.add(puzzle[row][col]);
+        }
 
-        for(int row = 0; row <= 2; row++){
-            for(int col = 0; col <= 2; col++){
-                int length = gridLeft.size();
-                int index = rand.nextInt(length);
-                int digit = gridLeft.get(index);
-                puzzle[row][col] = digit;
-                removeDigit(gridLeft, digit);
-                
+        return rowUsed;
+    }
+
+
+    public List<Integer> colUsed(int col){
+        List<Integer> colUsed = new ArrayList<>();
+        
+        for(int row = 0; row <= 8; row++){
+            colUsed.add(puzzle[row][col]);
+        }
+
+        return colUsed;
+    }
+
+    public List<Integer> possColumns(int row, int colLeft){
+        List<Integer> clearCols = new ArrayList<>();
+
+        for(int col = colLeft; col <= colLeft+2; col++){
+            int digitAtSpot = puzzle[row][col];
+            if(digitAtSpot == 0){
+                clearCols.add(col);
             }
         }
+
+        return clearCols;
     }
 
 
-    // method: fullList (List<Integer>)
-    // purpose: creates a list of numbers 1-9, used for grids
-    // parameters: none
-    public List<Integer> fullList(){
-        List<Integer> fullList = new ArrayList<>();
-        for(int i = 1; i <= 9; i++){
-            fullList.add(i);
-        }
-        return fullList;
-    }
+    public List<Integer> possRows(int rowTop, int colLeft){
+        List<Integer> clearRows = new ArrayList<>();
 
-
-    // method: removeDigit (void)
-    // purpose: removes a specified digit from the list
-    // parameters:  (1) list (List<Integer>): list to remove from
-    //              (2) digit (int): digit to be removed
-    public static void removeDigit(List<Integer> list, int digit){
-        for(int index = 0; index <= list.size(); index++){
-            int number = list.get(index);
-            if(number == digit){
-                list.remove(index);
-                return;
+        for(int row = rowTop; row <= rowTop+2; row++){
+            for(int col = colLeft; col <= colLeft+2; col++){
+                int digit = puzzle[row][col];
+                if(digit == 0){
+                    clearRows.add(row);
+                    break;
+                }
             }
         }
+
+        return clearRows;
     }
 
-    public void printGrid(){
-        for(int row = 0; row <= 2; row++){
-            for(int col = 0; col <= 2; col++){
-                System.out.print(puzzle[row][col] + " ");
+
+    public void printPuzzle(){
+        for(int row = 0; row <= 8; row++){
+            if(row % 3 == 0){
+                System.out.println();
+            }
+            for(int col = 0; col <= 8; col++){
+                if(col % 3 == 0){
+                    System.out.print(" ");
+                }
+                System.out.print(puzzle[row][col]);
             }
             System.out.println();
         }
@@ -74,4 +103,4 @@ public class Sudoku {
 }
 
 
-       
+    
